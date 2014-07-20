0c09c8ca3220a338f9add4b1d970af63844a0ea1
João Pinheiro <joaopinheiro@MacBook-Pro-de-Sistema-2.local>
2014-07-20 00:43:33 -0300

    Initial Commit

---
+++ Git Test/JLLPAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  JLLPAppDelegate.h
+//  Git Test
+//
+//  Created by João Pinheiro on 20/07/14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JLLPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
