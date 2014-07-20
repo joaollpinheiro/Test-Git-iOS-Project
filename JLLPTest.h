edcd8a072e2df488a73b750c23cb366f6428e510
João Pinheiro <joaopinheiro@MacBook-Pro-de-Sistema-2.local>
2014-07-20 00:59:22 -0300

    added a test class

---
+++ JLLPTest.h
@@ -0,0 +1,13 @@
+//
+//  JLLPTest.h
+//  Git Test
+//
+//  Created by João Pinheiro on 20/07/14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface JLLPTest : NSObject
+
+@end
---
+++ JLLPTest.m
@@ -0,0 +1,13 @@
+//
+//  JLLPTest.m
+//  Git Test
+//
+//  Created by João Pinheiro on 20/07/14.
+//  Copyright (c) 2014 CC. All rights reserved.
+//
+
+#import "JLLPTest.h"
+
+@implementation JLLPTest
+
+@end
